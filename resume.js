0466bc1dbecb6bd0fba870478e8fb261723bbf2b
Barvee2429 <132327127+Barvee2429@users.noreply.github.com>
2024-06-29 01:00:43 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,96 @@
+let myResume={
+  "basics": {
+    "name": "BARVEEN BABI R",
+    "email": "rbarveeraheem@gmail.com",
+    "phone": 9527417219,
+    "degree": "B.E",
+    "location": {
+      "address": "22,kamban nagar,pudukottai",
+      "postalCode": 622001,
+      "city": "Pudukottai",
+      "state": "Tamilnadu",
+      "country": "India"
+    },
+      },
+  "work": [
+    {
+      "company": "Sri Swathika Developers",
+      "position": "Admin Executive",
+      "startDate": "2020-11-04",
+      "endDate": "2021-08-15",
+    },
+  ],
+  "education": [
+    {
+      "institution": "Saveetha Engineering College",
+      "department": "Computer Science",
+      "studyType": "fulltime",
+      "batch start year": 2012,
+      "batch end year": 2016,
+      "gpa": 8.5,
+    }
+  ],
+  "skills": 
+    {
+      "name": "javascript",
+      "level": "beginner",
+      
+    },
+  
+  "languages": [
+    {
+      "language": "Tamil,English",
+    }
+  ],
+  "interests": [
+    {
+      "name": "Web Designing",
+    }
+  ]
+}
+console.log(myResume);
+
+
+
+/************for the given JSON iterate over all for loops****************/
+
+
+// //for in loop
+//  var mydetails = {
+// "Name":"Barveen Babi",
+// "Age":28,
+// "Degree":"B.E",
+// "Address":"Pudukottai",
+//  "Number":"+919677058343",
+// };
+
+// for (var i in mydetails){
+// console.log(i,mydetails[i]);
+// }
+
+
+// //for loop
+// var mydetails1 = {
+// "Name":"Barveen Babi",
+// "Age":28,
+// "Degree":"B.E",
+// "Address":"Pudukottai",
+//  "Number":"+919677058343",
+// };
+
+// for(var i=0;i<mydetails1.length;i++){
+// console.log(mydetails1[i]);
+    
+//  }
+
+// //for of
+
+// const result23 = [10,20,30,40,50]
+// for(var newresult23 of result23){
+//     console.log(newresult23);
+// }
+
+//  //for each
+// var result24 = [100,101,102,103,104,105]
+//  result24.forEach(numbers=>{console.log(numbers)});
+
